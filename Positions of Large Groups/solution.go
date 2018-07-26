8843eec8c426e8dd098466a3ac2ffd37796c8d0a
liwei <liwei@memebox.com>
2018-07-26 16:19:31 -0700

    new

---
+++ Positions of Large Groups/solution.go
@@ -0,0 +1,16 @@
+package main
+
+func largeGroupPositions(S string) [][]int {
+	result := [][]int{}
+	start := 0
+	for i := 0; i < len(S); i++ {
+		if i == len(S)-1 || S[i] != S[i+1] {
+			if i-start >= 2 {
+				result = append(result, []int{start, i})
+			}
+			start = i + 1
+		}
+	}
+	return result
+
+}
